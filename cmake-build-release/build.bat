9c6f6fe10b1a16dae6ab61f694302c2a6b099ac0
Adam Ellouze <elzadam11@tutamail.com>
2025-01-03 21:19:16 +0100

    Delete cmake-build-release directory
    Signed-off-by: Adam Ellouze <elzadam11@tutamail.com>

--- cmake-build-release/build.bat
+++
@@ -1,5 +0,0 @@
-del /F /Q vdl_test.exe 
-del /F /Q cmake-build-release/libVDL.dll 
-del /F /Q cmake-build-release/libVDL.dll.a 
-C:/Users/Adam/AppData/Local/Programs/CLion/bin/cmake/win/x64/bin/cmake.exe --build C:/Users/Adam/CLionProjects/vdl/cmake-build-release --target VDL -j 3 
-C:/Users/Adam/AppData/Local/Programs/CLion/bin/cmake/win/x64/bin/cmake.exe --build C:/Users/Adam/CLionProjects/vdl/cmake-build-release --target vdl_test -j 3
